16ef20a931cb42c4e8460b15b93a66a51efeae56
Marie <r19233@student.osu.cz>
2021-11-22 23:51:30 +0100

    Added menu above project folder

---
+++ my_new_plugin_again/src/main/java/cz/osu/kip/ProjectMenuAction.java
@@ -0,0 +1,27 @@
+package cz.osu.kip;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.pom.Navigatable;
+import org.jetbrains.annotations.NotNull;
+
+public class ProjectMenuAction  extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+        FormWindow.showFormWindow();
+
+//        Project currentProject = e.getProject();
+//        StringBuffer dlgMsg = new StringBuffer(e.getPresentation().getText() + " Selected!");
+//        String dlgTitle = e.getPresentation().getDescription();
+//        // If an element is selected in the editor, add info about it.
+//        Navigatable nav = e.getData(CommonDataKeys.NAVIGATABLE);
+//        if (nav != null) {
+//            dlgMsg.append(String.format("\nSelected Element: %s", nav.toString()));
+//        }
+//        Messages.showMessageDialog(currentProject, dlgMsg.toString(), dlgTitle, Messages.getInformationIcon());
+    }
+}
